d340f19114a0e8efd6919742741310f4dc6808ce
Dariru <daryl.tiamps@gmail.com>
2024-10-05 01:03:52 +0800

    wip add blog preview contents and  styling

--- src/components/BlogPreviewCard.jsx
+++ src/components/BlogPreviewCard.jsx
@@ -18,26 +18,27 @@ const BlogPreviewCard = () => {
                 className="article-image rounded-[10px]"
               />
               {/* blog preview contents */}
-              <div className="flex flex-col">
+              <div className="flex flex-col gap-[12px]">
                 <button
                   type="button"
-                  className="bg-yellow-950 justify-center items-center font-extrabold px-[12px] py-[4px] xs:w-[73px] xs:h-[26px] sm:w-[82px] sm:h-[29px] xs:text-[12px ] sm:text-[14px] rounded-[4px] px-["
+                  className="bg-yellow-950  w-[200px] justify-center items-center font-extrabold px-[12px] py-[4px] xs:w-[73px] xs:h-[26px] sm:w-[82px] sm:h-[29px] xs:text-[12px ] sm:text-[14px] rounded-[4px]"
                 >
                   Learnings
                 </button>
                 <p>Publised 21 Dec 2023</p>
-                <h1 className="font-sem">HTML & CSS Foundations</h1>
+                <h1 className="font-extrabold text-[24px] xs:text-[20px] sm:[text-[24px]">
+                  HTML & CSS Foundations
+                </h1>
 
                 <p>
                   These languages are the backbone of every website, defining
                   structure, content and presentation.
                 </p>
-
-                {/* blogger */}
-                <div className="flex gap-3 items-center">
-                  <img src={avatarImg} alt="user-image" width="32px" />
-                  <span>Greg Hooper</span>
-                </div>
+              </div>
+              {/* blogger */}
+              <div className="flex gap-3 items-center py-[]">
+                <img src={avatarImg} alt="user-image" width="32px" />
+                <span>Greg Hooper</span>
               </div>
             </div>
           </div>
